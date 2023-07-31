dd1c8fdd14020fb6a6207503e440b1d93703e3ec
Elioby <elioby@users.noreply.github.com>
2023-07-31 16:19:02 +0100

    feat: github actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,32 +7,6 @@ plugins {
 group = "ltd.lemongaming"
 version = "1.5.0"
 
-val nmsProject = project(":nms:nms-impl")
-dependencies {
-    // include all impls
-    for (child in nmsProject.childProjects) {
-        implementation(project(child.value.path, configuration = "reobf"))
-    }
-
-    implementation(project(":api"))
-    implementation(project(":nms:nms-interface"))
-}
-
-tasks {
-    shadowJar {
-        dependencies {
-            relocate("org.bstats", "com.owen1212055.${rootProject.name}.libs.bstats")
-        }
-
-        archiveClassifier.set("")
-        archiveBaseName.set("${project.name}")
-    }
-
-    assemble {
-        dependsOn("shadowJar")
-    }
-}
-
 allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
@@ -53,6 +27,17 @@ allprojects {
         publications {
             create<MavenPublication>("mavenJava") {
                 from(components["java"])
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/LemonGamingLtd/ParticleHelper")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
             }
         }
     }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    id("com.github.johnrengelman.shadow") version "7.1.0"
     id("xyz.jpenilla.run-paper") version "2.0.1"
     `maven-publish`
 }
@@ -23,8 +24,21 @@ tasks {
         options.release.set(17)
     }
 
+    shadowJar {
+        dependencies {
+            relocate("org.bstats", "com.owen1212055.${rootProject.name}.libs.bstats")
+        }
+
+        archiveClassifier.set("")
+        archiveBaseName.set("${parent?.project?.name}-${project.name}")
+    }
+
     jar {
         archiveBaseName.set("${parent?.project?.name}-${project.name}")
+    }
+
+    assemble {
+        dependsOn("shadowJar")
     }
 
     runServer {
