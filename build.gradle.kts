34479c851b0736a2e146246160a97b1cfa097a89
Elioby <elioby@users.noreply.github.com>
2023-07-31 16:13:50 +0100

    feat: shadow base project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,41 @@
 plugins {
     java
     id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 group = "ltd.lemongaming"
 version = "1.5.0"
 
-subprojects {
+val nmsProject = project(":nms:nms-impl")
+dependencies {
+    // include all impls
+    for (child in nmsProject.childProjects) {
+        implementation(project(child.value.path, configuration = "reobf"))
+    }
+
+    implementation(project(":api"))
+    implementation(project(":nms:nms-interface"))
+}
+
+tasks {
+    shadowJar {
+        dependencies {
+            relocate("org.bstats", "com.owen1212055.${rootProject.name}.libs.bstats")
+        }
+
+        archiveClassifier.set("")
+        archiveBaseName.set("${project.name}")
+    }
+
+    assemble {
+        dependsOn("shadowJar")
+    }
+}
+
+allprojects {
     apply(plugin = "java")
+    apply(plugin = "maven-publish")
     java {
         toolchain.languageVersion.set(JavaLanguageVersion.of(17))
     }
@@ -19,5 +47,13 @@ subprojects {
 
     dependencies {
         compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+            }
+        }
     }
 }
--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.1.0"
     id("xyz.jpenilla.run-paper") version "2.0.1"
     `maven-publish`
 }
@@ -18,52 +17,14 @@ dependencies {
     implementation(project(":nms:nms-interface"))
 }
 
-publishing {
-    publications {
-        publications {
-            create<MavenPublication>("mavenJava") {
-                from(components["java"])
-            }
-        }
-
-        tasks.withType<GenerateModuleMetadata> {
-            enabled = false
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/LemonGamingLtd/ParticleHelper")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-}
-
 tasks {
     compileJava {
         options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
 
-    shadowJar {
-        dependencies {
-            relocate("org.bstats", "com.owen1212055.${rootProject.name}.libs.bstats")
-        }
-
-        archiveClassifier.set("")
-        archiveBaseName.set("${parent?.project?.name}-${project.name}")
-    }
-
     jar {
         archiveBaseName.set("${parent?.project?.name}-${project.name}")
-    }
-
-    assemble {
-        dependsOn("shadowJar")
     }
 
     runServer {
